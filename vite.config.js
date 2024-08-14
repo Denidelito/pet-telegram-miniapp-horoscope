29318c62cae7589b0205d8b9bef5b833574ce289
Semen Belov <cool.gsmes@yandex.ru>
2024-08-14 14:31:26 +0300

    Поправил сборку Vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue'
-import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import vue from '@vitejs/plugin-vue';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
-  base:  './',
-  publicDir: 'docs',
+  base: './',
+  build: {
+    outDir: resolve(__dirname, 'docs'),
+    emptyOutDir: true,
+  },
+  publicDir: resolve(__dirname, 'src/assets/'),
   plugins: [
     createSvgIconsPlugin({
       iconDirs: [resolve(process.cwd(), 'src/assets/icons/')],
       symbolId: 'icon-[name]',
     }),
-    vue()
+    vue(),
   ],
   server: {
     historyApiFallback: true,
